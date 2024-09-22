acc0f686219ff1302c6fd1482e3ba7f23c3287ea
cocoorange56 <wood-stone56@126.com>
2024-09-22 21:18:56 +0800

    common_inc.h add comment240922

--- dummy-35motor-fw/UserApp/common_inc.h
+++ dummy-35motor-fw/UserApp/common_inc.h
@@ -1,6 +1,11 @@
-#ifndef LOOP_H  /ifndef条件编译预编译命令
+#ifndef LOOP_H  //#ifndef...#endif,条件编译预编译命令,防止头文件的重复包含和编译.
+/*如果一个项目里有两个.c文件（A/B）同时include了同一个头文件（H），而编译时，这两个.c文件要一同编译成一个可运行文件，
+ * 在文件A中include了头文件H，在文件B中再次include头文件的话，会产生声明冲突。而将头文件内容放到
+ * #ifndef
+ * #endif中，会在include之前判断该头文件是否已经被include过，避免重复引用。*/
 #define LOOP_H
-
+/*
+ * */
 #ifdef __cplusplus
 extern "C" {
 #endif
